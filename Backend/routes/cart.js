5d667771aed69e14266fa2f140a1f5ed9f8aef04
Lokesh <lokeshsingh7695@gmail.com>
2025-03-26 12:31:57 +0530

    Add delete route for deleting Individual Items

--- Backend/routes/cart.js
+++ Backend/routes/cart.js
@@ -18,6 +18,7 @@ router.get("/getall", async(req, res) => {
 })
 router.post("/create", async(req, res) => {
     const data = req.body;
+    console.log(req.body);
     try{
         const cart = new cartModel(data);
         await cart.save();
@@ -33,6 +34,19 @@ router.delete("/deletecards", async(req, res) => {
     try{
         await cartModel.deleteMany();
         res.send("All cards deleted");
+    }
+    catch(err){
+        res.status(500).send({message: err.message});
+        console.error(err);
+    }
+})
+router.delete("/delete/:id", async(req, res) => {
+    try{
+        const cart = await cartModel.findByIdAndDelete(req.params.id);
+        if(!cart){
+            return res.status(404).send({message: "Card not found"});
+        }
+        res.send(cart);
     }
     catch(err){
         res.status(500).send({message: err.message});
